881061daf4d2bb60e7be679ebcdeeab9dec1d0b8
gpovidaiko <gpovidaiko@hotmail.com>
2021-07-04 14:42:29 -0300

    Refatoração da classe Main

--- src/lab/gpovidaiko/wakeup/Main.java
+++ src/lab/gpovidaiko/wakeup/Main.java
@@ -7,29 +7,28 @@ import java.awt.Robot;
 
 public class Main {
 
-    private static final int FIVE_SECONDS = 5000;
+	private static final int ONE_MINUTE = 60000;
+	private static final int COORD_SHIFT = 5;
 
-    public static void main(String[] args) throws AWTException, InterruptedException {
-        while (true) {
-            Point location = Main.getPointerLocation();
+	public static void main(String[] args) throws AWTException, InterruptedException {
+		while (true) {
+			Main.shakePointer();
 
-            Main.setPointerLocation(location.x + 5, location.y + 5);
-            // Main.setPointerLocation(location.x, location.y);
-
-            Thread.sleep(FIVE_SECONDS);
-        }
+			Thread.sleep(ONE_MINUTE);
+		}
 	}
 
-    private static Point getPointerLocation() {
-        Point location = MouseInfo.getPointerInfo().getLocation();
+	private static void shakePointer() throws AWTException {
+		Point location = MouseInfo.getPointerInfo().getLocation();
 
-        return location;
-    }
-    
-    private static void setPointerLocation(int x, int y) throws AWTException {
-        Robot robot = new Robot();
+		Main.setPointerLocation(location.x + COORD_SHIFT, location.y + COORD_SHIFT);
+		Main.setPointerLocation(location.x, location.y);
+	}
 
-        robot.mouseMove(x, y);
-    }
-    
+	private static void setPointerLocation(int x, int y) throws AWTException {
+		Robot robot = new Robot();
+
+		robot.mouseMove(x, y);
+	}
+
 }
